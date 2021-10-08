438cd0443e3bbafddc1f5c146ae78141dd35dfad
brandonkodehode <89029540+brandonkodehode@users.noreply.github.com>
2021-10-08 12:33:08 +0200

    Update conditionaltraining.js

---
+++ conditionaltraining.js
@@ -0,0 +1,66 @@
+// let isLoaded = true
+
+// if (isLoaded === true) {
+//     console.log("your data is loaded and ready to be used")
+// } else {
+//     console.log("your data needs more time to load or another error occured")
+// }
+
+// assignment 3
+
+// var age = 13
+// let message = ""
+
+// if (age <= 17){
+//     message = "too young to code"
+// } else if (age === 18){
+//     message = "perfect age to code"
+// } else (age >= 19) {
+//     message = "probably dead"
+// }
+
+// console.log(message)
+
+// assingment 4
+
+// var string = ""
+
+// if (string = "") {
+//     window.alert("string needs to be filled")
+// } else {
+//     console.log(string)
+// }
+
+// assignment 5
+
+// let worldIsEnding = false
+// let year = 2021
+// let message = ""
+
+// if (worldIsEnding =true && year < 2021){
+//     message = "we still have some time to save it"
+// } else if (worldIsEnding = true && year === 2021){
+//     message = "the world is ending!"
+// } else {
+//     message = "no no i think we're good"
+// }
+
+// console.log(message)
+
+// assingment 6
+
+// let time = 12
+// let day = 7
+// let message = ""
+
+// if (day === 5 || time === 15){
+//     message = "small happy"
+// } else if (day === 5 && time === 15){
+//     message = "big happy"
+// } else if (day > 5 || time > 15){
+//     message = "continue happy"
+// } else {
+//     message = "big sad"
+// }
+
+// console.log(message)
