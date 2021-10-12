f8429e0a385c8673a73ac1e5ab5065143f690a1f
brandonkodehode <89029540+brandonkodehode@users.noreply.github.com>
2021-10-12 15:53:19 +0200

    Update java-course/java.js

--- java-course/java.js
+++ java-course/java.js
@@ -7,8 +7,12 @@ let HasBlackJack = false
 let IsAlive = true
 let message = ""
 let messageEL = document.getElementById("message-el")
+let sumEL = document.getElementById("sum-el")
+let cardsEL = document.getElementById("cards-el")
 
 function startGame(){
+    sumEL.textContent = "sum: " + sum
+    cardsEL.textContent ="Cards: " + firstcard + " " + secondcard
     if (sum <= 20) {
         message = "do you want a new card?"
     } else if (sum === 21){
